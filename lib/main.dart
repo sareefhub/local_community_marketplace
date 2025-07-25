3e11ea09f3fa6c438cd1f21a4e7b578706f6e495
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2025-07-25 17:25:10 +0700

    setup: connect Firebase.initializeApp with DefaultFirebaseOptions for all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 // import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
